65b99241fd45502fb488b2a224f79083a0e46b38
Brett Connolly <Brett@Bretts-MacBook-Pro.local>
2016-04-22 20:25:32 -0400

    stuff

--- server.js
+++ server.js
@@ -32,8 +32,10 @@ var app = express(),
 	  })
 	});
 
-	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
-	app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
+	var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+	var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
 	//Open server for socket
-	server.listen(app.get('port'), app.get('ip'), function() {});
+	server.listen(server_port, server_ip_address, function () {
+		console.log("Listening on " + server_ip_address + ", server_port " + server_port);
+	});
