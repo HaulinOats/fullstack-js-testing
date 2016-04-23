b404f6927bdd5dd6f521ac2c896c1e6ff7957c2f
Brett Connolly <Brett@Bretts-MacBook-Pro.local>
2016-04-22 20:05:50 -0400

    stuff

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ var app = express(),
 	  })
 	});
 
-	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
+	app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
 	app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 	//Open server for socket
