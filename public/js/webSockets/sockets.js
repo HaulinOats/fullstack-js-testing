f595f3277a509dddf358d8a5f93cc30fb5197b50
Brett Connolly <Brett@Bretts-MacBook-Pro.local>
2016-04-25 20:27:31 -0400

    updates to chat app

--- public/js/webSockets/sockets.js
+++ public/js/webSockets/sockets.js
@@ -1,15 +1,26 @@
-var socket = io();
+var socket = io(),
+	userId;
 socket.on('openConnection', function (data) {
-	$('#chat-window').append('<p id="open-connection">'+ data.hello +'</p>');
+	$('#chat-window').append('<p id="open-connection">Connection opened...</p>');
+	$('#chat-window').append('<p>Your ID is: ' + data.userId + '</p>');
+	$('#active-users-count').text(data.activeUsers);
+	userId = data.userId;
 });
-socket.on('returnChat', function(data){
-	$('#chat-window').append('<p><b>Server: </b> '+ data.message +'</p>');
+socket.on('addToChat', function(data){
+	$('#chat-window').append('<p><b>' + data.userId + ':</b>  ' + data.message + '</p>');
 	$('#chat-window').scrollTop(10000000);
-})
+});
+socket.on('user_joined', function(data){
+	$('#active-users-count').text(data.activeUsers);
+	$('#chat-window').append('<p>Your ID is: ' + data.userId + ' has joined the conversation...</p>');
+});
+socket.on('user_left', function(data){
+	$('#active-users-count').text(data.activeUsers);
+	$('#chat-window').append('<p>Your ID is: ' + data.userId + ' has left the conversation</p>');
+});
 $('#chat-submit').on('keyup', function(event){
 	if (event.keyCode === 13 && event.currentTarget.value.length > 0) {
-		socket.emit('clientSent', {message:event.currentTarget.value});
-		$('#chat-window').append('<p><b>Client: </b> '+ event.currentTarget.value +'</p>');
+		socket.emit('message', {message:event.currentTarget.value});
 		$(event.currentTarget).val('');
 	}
-})
+});
--- server.js
+++ server.js
@@ -17,20 +17,36 @@ var express = require('express'),
 	io = require('socket.io')(server);
 
 	app.set('view engine', 'pug');
-  app.use( express.static(__dirname+'/public') );
+  	app.use( express.static(__dirname+'/public') );
 	app.set('views', __dirname + '/templates');
 	require('./routes')(app);
 
+	//bacon ipsom for chat population
+	var socketResArr = ["Bacon","ipsum","dolor","amet","chuck","pork","belly","turkey","ham","meatball","boudin","Ball","tip","fatback","porchetta,","turkey","pork","chop","leberkas","sausage","Pancetta","picanha","brisket","tri-tip","filet","mignon","rump","doner","strip","steak","ball","tip","andouille","Strip","steak","pork","belly","capicola","frankfurter","Picanha","jowl","andouille","turkey","meatloaf","chicken"];
+	
 	//Socket handling
-	var socketResArr = ["Bacon","ipsum","dolor","amet","chuck","pork","belly","turkey","ham","meatball","boudin.","Ball","tip","fatback","porchetta,","turkey","pork","chop","leberkas","sausage.","Pancetta","picanha","brisket","tri-tip","filet","mignon","rump","doner","strip","steak","ball","tip","andouille.","Strip","steak","pork","belly","capicola","frankfurter.","Picanha","jowl","andouille","turkey","meatloaf","chicken"];
-	io.sockets.on('connection', function(socket) {
-		//confirm connection
-		socket.emit('openConnection', { hello:'Connection opened...'});
+	io.on('connect', function(socket) {
+		//generate user id based on end of current timestamp combined with a bacon ipsum word
+		var d = new Date(),
+			timeStr = d.getTime().toString(),
+			userId = socketResArr[Math.floor(Math.random() * socketResArr.length)].toUpperCase() + timeStr.slice(10);
+
+		//send user their unique ID and number of users in chat
+		socket.emit('openConnection', {"activeUsers":socket.server.eio.clientsCount, "userId":userId});
+
+		//broadcast new count to all users
+		socket.broadcast.emit('user_joined', {"userId": userId, "activeUsers":socket.server.eio.clientsCount});
 
 		//return message sent from client back to client
-	  socket.on('clientSent', function(data){
-	  	socket.emit('returnChat', {message:socketResArr[Math.floor(Math.random() * socketResArr.length)]});
-	  })
+		socket.on('message', function(data){
+			// socket.emit('addToChat', {message:data.message});
+			io.sockets.emit('addToChat', {userId: userId, message:data.message});
+		})
+
+		//on disconnect
+		socket.on('disconnect', function(){
+			socket.broadcast.emit('user_left', {"userId":userId, "activeUsers":socket.server.eio.clientsCount});
+		});
 	});
 
 	var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
